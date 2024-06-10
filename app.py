2ed729f428eee75417edce6e52ef5fcbb71a8f76
xke <xke@alum.mit.edu>
2024-06-10 14:06:10 -0700

    allow users to select agents to use

--- app.py
+++ app.py
@@ -24,102 +24,112 @@ from datetime import date
 weave.init('finbot-crewai-streamlit')
 
 @weave.op()
-def process_user_input(company, chosen_llm, historical_horizon_in_years, prediction_time_horizon_in_years):
+def process_user_input(company, chosen_llm, historical_horizon_in_years, prediction_time_horizon_in_years,
+                       news_analysis_agent_enabled, sec_filings_agent_enabled, technical_indicators_agent_enabled):
 
     st.session_state.messages.append({"role": "user", "content": company})
     st.chat_message("user").write(company)
 
-
     # Define tasks to run, and the agent to run it with
 
-    analyze_news_task = Task(
-        description=dedent(f"""
-            Collect and summarize important news articles, press
-            releases, and market analyses related to the company and
-            its industry. Look for the most recent news possible, but also
-            include any historical news that is very relevant to the future
-            prospects of the company.
+    tasksList = []
+    agentsList = []
 
-            Selected company by the customer: {company}
-            Today's date: {date.today()}
-            Historical time horizon for news: {historical_horizon_in_years} years
-        """),
-        expected_output=dedent(f"""
-            Your output is a concise report that includes a
-            summary of the latest news, any notable
-            shifts in market sentiment, and impacts on
-            the future of the company.
+    if news_analysis_agent_enabled:
+        agentsList.append(news_analysis_agent)
+        analyze_news_task = Task(
+            description=dedent(f"""
+                Collect and summarize important news articles, press
+                releases, and market analyses related to the company and
+                its industry. Look for the most recent news possible, but also
+                include any historical news that is very relevant to the future
+                prospects of the company.
 
-            Also include in the output the company's stock ticker symbol,
-            whether you believe the company's stock price will increase
-            in the next {prediction_time_horizon_in_years} years, and the confidence
-            level of your prediction/belief."""),
-        agent=news_analysis_agent
-    )
-    
-    analyze_sec_filings_task = Task(
-        description=dedent(f"""
-            Analyze the latest 10-Q and 10-K filings from EDGAR for
-            the company in question.
-            Focus on key sections like Management's Discussion and
-            Analysis, financial statements, insider trading activity,
-            and any disclosed risks.
-            Extract relevant data and insights that could influence
-            the company's future performance.
+                Selected company by the customer: {company}
+                Today's date: {date.today()}
+                Historical time horizon for news: {historical_horizon_in_years} years
+            """),
+            expected_output=dedent(f"""
+                Your output is a concise report that includes a
+                summary of the latest news, any notable
+                shifts in market sentiment, and impacts on
+                the future of the company.
 
-            Selected company by the customer: {company}
-            Today's date: {date.today()}
-        """),
-        expected_output=dedent(f"""
-            Your output is a concise report that
-            highlights significant findings from these filings,
-            including any red flags or positive indicators for your
-            customer.
+                Also include in the output the company's stock ticker symbol,
+                whether you believe the company's stock price will increase
+                in the next {prediction_time_horizon_in_years} years, and the confidence
+                level of your prediction/belief."""),
+            agent=news_analysis_agent
+        )
+        tasksList.append(analyze_news_task)
 
-            Also include in the output the company's stock ticker symbol,
-            whether you believe the company's stock price will increase
-            in the next {prediction_time_horizon_in_years} years, and the confidence
-            level of your prediction/belief."""),
-        agent=sec_filings_agent
-    )
+    if sec_filings_agent_enabled:
+        agentsList.append(sec_filings_agent)
+        analyze_sec_filings_task = Task(
+            description=dedent(f"""
+                Analyze the latest 10-Q and 10-K filings from EDGAR for
+                the company in question.
+                Focus on key sections like Management's Discussion and
+                Analysis, financial statements, insider trading activity,
+                and any disclosed risks.
+                Extract relevant data and insights that could influence
+                the company's future performance.
 
-    # analyze_technical_indicators_task = Task(
-    #     description=dedent(f"""
-    #         [update prompt]
+                Selected company by the customer: {company}
+                Today's date: {date.today()}
+            """),
+            expected_output=dedent(f"""
+                Your output is a concise report that
+                highlights significant findings from these filings,
+                including any red flags or positive indicators for your
+                customer.
 
-    #         Selected company by the customer: {company}
-    #         Today's date: {date.today()}
-    #     """),
-    #     expected_output=dedent(f"""
-    #         Your output is a concise report that
-    #         highlights significant findings from these filings,
-    #         including any red flags or positive indicators for your
-    #         customer.
+                Also include in the output the company's stock ticker symbol,
+                whether you believe the company's stock price will increase
+                in the next {prediction_time_horizon_in_years} years, and the confidence
+                level of your prediction/belief."""),
+            agent=sec_filings_agent
+        )
+        tasksList.append(analyze_sec_filings_task)
 
-    #         Also include in the output the company's stock ticker symbol,
-    #         whether you believe the company's stock price will increase
-    #         in the next {prediction_time_horizon_in_years} years, and the confidence
-    #         level of your prediction/belief."""),
-    #     agent=technical_indicators_agent
-    #  )
+    # TODO: to enable this functionality
+    # if technical_indicators_agent_enabled:
+    #     agentsList.append(technical_indicators_agent)
+    #     analyze_sec_filings_task = Task(
+    #         description=dedent(f"""
+    #             Analyze the latest 10-Q and 10-K filings from EDGAR for
+    #             the company in question.
+    #             Focus on key sections like Management's Discussion and
+    #             Analysis, financial statements, insider trading activity,
+    #             and any disclosed risks.
+    #             Extract relevant data and insights that could influence
+    #             the company's future performance.
+
+    #             Selected company by the customer: {company}
+    #             Today's date: {date.today()}
+    #         """),
+    #         expected_output=dedent(f"""
+    #             Your output is a concise report that
+    #             highlights significant findings from these filings,
+    #             including any red flags or positive indicators for your
+    #             customer.
+
+    #             Also include in the output the company's stock ticker symbol,
+    #             whether you believe the company's stock price will increase
+    #             in the next {prediction_time_horizon_in_years} years, and the confidence
+    #             level of your prediction/belief."""),
+    #         agent=sec_filings_agent
+    #     )
+    #     tasksList.append(analyze_technical_indicators_task)
 
     # Set up the crew and process tasks hierarchically
-
     project_crew = Crew(
-        tasks=[analyze_news_task, analyze_sec_filings_task],
-        agents=[news_analysis_agent, sec_filings_agent],
+        tasks=tasksList,
+        agents=agentsList,
         process=Process.hierarchical,
         manager_llm=chosen_llm,
         manager_callbacks=[CustomHandler("Manager")]
     )
-
-    # project_crew = Crew(
-    #     tasks=[analyze_news_task, analyze_sec_filings_task, analyze_technical_indicators_task],
-    #     agents=[news_analysis_agent, sec_filings_agent, technical_indicators_agent],
-    #     process=Process.hierarchical,
-    #     manager_llm=chosen_llm,
-    #     manager_callbacks=[CustomHandler("Manager")]
-    # )
 
     report = project_crew.kickoff()
     return report
@@ -153,13 +163,39 @@ prediction_time_horizon_in_years = st.sidebar.number_input(
     value=0.5, min_value=0.0, max_value=10.0, step=0.5, format="%.1f"
 )
 
-# Main Streamlit UI setup
-st.title("Finbot: A Company Analysis Tool")
 
 # Define agents with their specific roles and goals
-news_analysis_agent = get_news_analysis_agent(chosen_llm)
-sec_filings_agent = get_sec_filings_agent(chosen_llm)
-#technical_indicators_agent = get_technical_indicators_agent(chosen_llm)
+
+st.sidebar.write("")
+st.sidebar.write("Choose CrewAI agent(s) to use:")
+
+news_analysis_agent_enabled = st.sidebar.checkbox(
+    'News analysis agent',
+    value=True
+)
+
+if news_analysis_agent_enabled:
+    news_analysis_agent = get_news_analysis_agent(chosen_llm)
+
+sec_filings_agent_enabled = st.sidebar.checkbox(
+    'SEC filings agent',
+    value=False
+)
+
+if sec_filings_agent_enabled:
+    sec_filings_agent = get_sec_filings_agent(chosen_llm)
+
+technical_indicators_agent_enabled = st.sidebar.checkbox(
+    'Technical indicators agent',
+    value=False
+)
+
+#if technical_indicators_agent_enabled:
+#    technical_indicators_agent = get_technical_indicators_agent(chosen_llm)
+
+
+# Main Streamlit UI setup
+st.title("Finbot: A Company Analysis Tool")
 
 # Initialize the message log in session state if not already present
 if "messages" not in st.session_state:
@@ -173,7 +209,8 @@ for msg in st.session_state.messages:
 # Handle user input
 if company := st.chat_input():
 
-    report = process_user_input(company, chosen_llm, historical_horizon_in_years, prediction_time_horizon_in_years)
+    report = process_user_input(company, chosen_llm, historical_horizon_in_years, prediction_time_horizon_in_years,
+                                news_analysis_agent_enabled, sec_filings_agent_enabled, technical_indicators_agent_enabled)
 
     # Display the final result
     result = f"##### Manager's Final Report: \n\n {report}"
